58c3b8ecb74fa3f8dae36bd367ac3153c7f7bf7f
Marcia Silva <contato.pix@gmail.com>
2019-02-19 12:12:22 -0300

    Estudo sobre "Props"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet />
+
+        <Greet name="Bruce" heroName="Batman">
+          <p>This is a children props.</p>
+        </Greet>
+
+        <Greet name="Clark" heroName="Superman">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Diana" heroName="Wonder Woman" />
+
+        <Welcome name="Bruce" heroName="Batman" />
+        <Welcome name="Clark" heroName="Superman" />
+        <Welcome name="Diana" heroName="Wonder Woman" />
       </div>
     );
   }
