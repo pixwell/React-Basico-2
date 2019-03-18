8bb2e8f80690cfb10c0494b6e9018cc8b23c4dd8
Marcia Silva <contato.pix@gmail.com>
2019-03-18 16:11:50 -0300

    Greet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-//import Greet from './components/Greet';
-//import Welcome from './components/Welcome';
+import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
 //import Message from './components/Message';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Counter />
+      <div className="App">        
         {/*
+        <Counter />
         <Message />
         <Greet name="Bruce" heroName="Batman">
           <p>This is a children props.</p>
@@ -24,8 +24,9 @@ class App extends Component {
 
         <Welcome name="Bruce" heroName="Batman" />
         <Welcome name="Clark" heroName="Superman" />
-        <Welcome name="Diana" heroName="Wonder Woman" />
-        */}        
+        
+        */}  
+        <Greet name="Diana" heroName="Wonder Woman" />      
       </div>
     );
   }
